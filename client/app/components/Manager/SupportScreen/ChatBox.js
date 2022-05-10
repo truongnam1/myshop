983439a674678dba036446629bab258be463564b
19020440 <63657552+19020440@users.noreply.github.com>
2022-05-10 23:48:45 +0700

    update chatbox

--- client/app/components/Manager/SupportScreen/ChatBox.js
+++ client/app/components/Manager/SupportScreen/ChatBox.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 
 const ENDPOINT =
   window.location.host.indexOf('localhost') >= 0
-    ? 'http://127.0.0.1:5000'
+    ? 'http://127.0.0.1:3000'
     : window.location.host;
 
 export default function ChatBox(props) {
